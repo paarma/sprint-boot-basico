fdf1beb16045bd8d2db80955f68ca5146876a7c6
pablo.manquillo <pablo.manquillo@ceiba.com.co>
2022-03-16 21:41:59 -0500

    Uso de variables definidas en el archivo de propiedades

--- src/main/java/com/fundamentos/sprintboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/sprintboot/fundamentos/FundamentosApplication.java
@@ -2,6 +2,7 @@ package com.fundamentos.sprintboot.fundamentos;
 
 import com.fundamentos.sprintboot.fundamentos.bean.MyBean;
 import com.fundamentos.sprintboot.fundamentos.bean.MyBeanWithDependency;
+import com.fundamentos.sprintboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentos.sprintboot.fundamentos.component.ComponentDependency;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
@@ -18,13 +19,16 @@ public class FundamentosApplication implements CommandLineRunner {
 	private ComponentDependency componentDependency;
 	private MyBean myBean;
 	private MyBeanWithDependency myBeanWithDependency;
+	private MyBeanWithProperties myBeanWithProperties;
 
 	//Se inyecta la dependencia en el constructor (Se inyecta la interfaz)
 	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
-								  MyBean myBean, MyBeanWithDependency myBeanWithDependency){
+								  MyBean myBean, MyBeanWithDependency myBeanWithDependency,
+								  MyBeanWithProperties myBeanWithProperties){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
+		this.myBeanWithProperties = myBeanWithProperties;
 	}
 
 	public static void main(String[] args) {
@@ -36,5 +40,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		componentDependency.saludar();
 		myBean.print();
 		myBeanWithDependency.printWithDependency();
+		System.out.println("Valores del archivo de propiedades: "+myBeanWithProperties.function());
 	}
 }
---
+++ src/main/java/com/fundamentos/sprintboot/fundamentos/configuration/GeneralConfiguration.java
@@ -0,0 +1,30 @@
+package com.fundamentos.sprintboot.fundamentos.configuration;
+
+import com.fundamentos.sprintboot.fundamentos.bean.MyBeanWithProperties;
+import com.fundamentos.sprintboot.fundamentos.bean.MyBeanWithPropertiesImplement;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeneralConfiguration {
+
+    /**
+     * Con "@Value()" podemos llamar a las variables que definimos en el archivo de propiedades
+     * application.properties
+     */
+    @Value("${value.nombre}")
+    private String nombre;
+
+    @Value("${value.apellido}")
+    private String apellido;
+
+    @Value("${value.random}")
+    private String random;
+
+    //Hacemos el llamado a un Bean para usar estos atributos (propiedades)
+    @Bean
+    public MyBeanWithProperties function(){
+        return new MyBeanWithPropertiesImplement(nombre, apellido);
+    }
+}
